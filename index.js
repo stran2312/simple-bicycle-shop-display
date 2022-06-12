19c275257a05963cc473d34bf067cfa9732dea86
Sang Tran <100749058+stran2312@users.noreply.github.com>
2022-06-12 02:29:10 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,58 @@
+const http = require('http');
+const url = require('url');
+const fs = require('fs').promises;
+const bicycles = require('./data/data.json');
+
+const server = http.createServer( async (req,res) => {
+    if(req.url === '/favicon.ico') return;
+    console.log('Sever is running');
+
+    const myURL = new URL(req.url, `http://${req.headers.host}/`);
+    const id = myURL.searchParams.get('id');
+    const pathName = myURL.pathname;
+
+    if(pathName === '/') {
+        const html = await fs.readFile('./view/bicycles.html', 'utf-8');
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html);
+
+    } else if (pathName === '/bicycle' && id >= 0 && id <= 5) {
+        let html = await fs.readFile('./view/overview.html', 'utf-8');
+        const bicycle = bicycles.find((b) => b.id === id);
+        
+        html = html.replace(/<%IMAGE%>/g, bicycle.image);
+        html = html.replace(/<%NAME%>/g, bicycle.name);
+
+        let price = bicycle.originalPrice;
+        if(bicycle.hasDiscount) {
+            price = (price* (100 - bicycle.discount)) / 100;
+        }
+        html = html.replace(/<%NEWPRICE%>/g, `$${price}`);
+
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(html);
+
+    } else if (/\.(png)$/i.test(req.url)) {
+        const image = await fs.readFile(`./public/img/${req.url.slice(1)}`);
+        res.writeHead(200, {'Content-Type': 'image/png'});
+        res.end(image);
+
+    } else if (/\.(css)$/i.test(req.url)) {
+        const css = await fs.readFile(`./public/css/index.css`);
+        res.writeHead(200, {'Content-Type': 'text/css'});
+        res.end(css);
+
+    } else if (/\.(svg)$/i.test(req.url)) {
+        const svg = await fs.readFile('./view/icons.svg');
+        res.writeHead(200, {'Content-Type': 'image/svg+xml'});
+        res.end(svg);
+    } 
+    
+    else {
+        res.writeHead(404, {'Content-Type': 'text/html'});
+        res.end('<div><h1>File not found</h1></div>');
+    }
+    
+});
+
+server.listen(3000);
